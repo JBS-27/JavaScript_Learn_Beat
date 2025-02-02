aea4e4cea802d3a051d9c7f0f45e95c54dd3f6fd
JBS-27 <singharshll72@gmail.com>
2025-02-02 21:29:55 +0530

    Add files via upload

---
+++ 03_ConversionOperation.js
@@ -0,0 +1,27 @@
+// let score = "33abc"
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // NaN because string cannot be converted into string 
+// suppose score=null then 0, score=undefined then NaN
+
+// "33":> 33
+// "33abc":> NaN
+// true :> 1; false:>0
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 :> true
+// "" :> false
+// "MahaKumbh" :> true
+
+let someNumber =23
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+// 23 converted into string 
